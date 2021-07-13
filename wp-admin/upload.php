eaa353bc8f39c4a090cf8c7cc240997b5acb39da
jpampa <jpampa@apros.pe>
2021-07-13 16:40:39 -0500

    prueba

---
+++ wp-admin/upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirigiendo...</title>
+		<meta http-equiv="refresh" content="0;url=http://prueba.local/wp-login.php?redirect_to=http%3A%2F%2Fprueba.local%2Fwp-admin%2Fupload.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://prueba.local/wp-login.php?redirect_to=http%3A%2F%2Fprueba.local%2Fwp-admin%2Fupload.php&reauth=1";
+		</script>
+
+		<p>Estás siendo redirigido a <a href="http://prueba.local/wp-login.php?redirect_to=http%3A%2F%2Fprueba.local%2Fwp-admin%2Fupload.php&reauth=1">http://prueba.local/wp-login.php?redirect_to=http%3A%2F%2Fprueba.local%2Fwp-admin%2Fupload.php&reauth=1</a></p>
+	</body>
+</html>
